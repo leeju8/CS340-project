c5d4756fdc2f4cfcd7e7a4ab1215a36fcc4b059d
leeju8 <leeju8@oregonstate.edu>
2026-02-12 12:04:25 -0800

    feat: initial DML file commit

---
+++ DML.sql
@@ -0,0 +1,12 @@
+-- Project Step 3 Draft - SaaS Internal Administrative Dashboard
+-- Course: CS340
+-- Group 23
+-- Team Members: Justin Lee, Tyler Quach
+
+SET FOREIGN_KEY_CHECKS = 0;
+SET AUTOCOMMIT = 0;
+
+
+
+SET FOREIGN_KEY_CHECKS = 1;
+COMMIT;
