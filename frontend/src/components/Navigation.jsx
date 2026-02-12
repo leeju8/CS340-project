ae24af65c90d19c666f1f6ad3255973e007edb7d
leeju8 <leeju8@oregonstate.edu>
2026-02-12 15:32:29 -0800

    feat: update anvigation component

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+function Navigation() {
+    return (
+        <nav>
+            Navigation:
+            <a href="/">Home</a>
+            <a href="/users">Users</a>
+            <a href="/invoices">Invoices</a>
+            <a href="/subscriptions">Subscriptions and Features</a>
+            <a href="/preferences">Preferences</a>
+        </nav>
+    )
+} export default Navigation;
